c31f09aa482635d42d13648e91e835be48fd0e95
Chris Arridge <chris.arridge@opendataservices.coop>
2025-06-08 15:22:12 +0100

    feat: add reporting organiation short name to dataset

--- custom/relationships/vardefs/iati_datasets_accounts_IATI_Datasets.php
+++ custom/relationships/vardefs/iati_datasets_accounts_IATI_Datasets.php
@@ -22,6 +22,18 @@ $dictionary["IATI_Datasets"]["fields"]["owner_org_name"] = array(
   'module' => 'Accounts',
   'rname' => 'name',
 );
+$dictionary["IATI_Datasets"]["fields"]["owner_org_short_name"] = array(
+  'name' => 'owner_org_short_name',
+  'type' => 'relate',
+  'source' => 'non-db',
+  'vname' => 'LBL_IATI_DATASETS_OWNER_ORG_FROM_ACCOUNTS_TITLE',
+  'save' => true,
+  'id_name' => 'iati_datasets_owner_orgaccounts_ida',
+  'link' => 'iati_datasets_owner_org',
+  'table' => 'accounts',
+  'module' => 'Accounts',
+  'rname' => 'iati_short_name',
+);
 $dictionary["IATI_Datasets"]["fields"]["iati_datasets_owner_orgaccounts_ida"] = array(
   'name' => 'iati_datasets_owner_orgaccounts_ida',
   'type' => 'link',
